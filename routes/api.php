b781147d6f53b606d3a145ab5ecd0595741eb855
Fatma43 <104324470+Fatma43@users.noreply.github.com>
2023-08-30 13:42:09 +0300

    next

--- routes/api.php
+++ routes/api.php
@@ -16,7 +16,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-
 Route::get('product',[ProductController::class,'index']);
 Route::post('product',[ProductController::class,'store']);
 Route::get('product/{id}',[ProductController::class,'show']);
